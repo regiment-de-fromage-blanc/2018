339b2a9e540e881935f7bdd3b59ba096e8ff6673
Thomas Peyrot <thomas.peyrot10@gmail.com>
2018-12-06 23:29:16 +0100

    Generation of Status elements from a json variable

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -4,23 +4,40 @@ import '../App.css';
 import './Status.css';
 
 class Status extends React.Component {
-  renderCol() {
+  renderCol = state => {
     return (
       <Col xs={12} className="statusCol">
-        <div className="statusName">Machine</div>
+        <div className="statusName">{state.name}</div>
+        <div className="statusStatus">{state.status}</div>
+        <div className="statusBattery">{state.battery}</div>
       </Col>
     );
-  }
+  };
+
   render() {
+    const states = [
+      {
+        name: 'Téléphone',
+        status: 'Opérationnel',
+        battery: '67%'
+      },
+      {
+        name: 'R2D2',
+        status: 'Opérationnel',
+        battery: '100%'
+      },
+      {
+        name: 'Détecteur de mouvement',
+        status: 'Non opérationnel',
+        battery: '0%'
+      }
+    ];
+
     return (
       <div className="App">
         <div className="App-status">
           <Grid>
-            <Row>
-              {this.renderCol()}
-              {this.renderCol()}
-              {this.renderCol()}
-            </Row>
+            <Row>{states.map(this.renderCol)}</Row>
           </Grid>
         </div>
       </div>
