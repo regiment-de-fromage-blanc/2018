c2138aa1cad2046023b1868b3c1ec453ce56705d
Edouard Menayde <edouard.menayde@gmail.com>
2018-12-06 20:17:32 +0100

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import {Provider} from "react-redux";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import configureStore from './store';
@@ -11,7 +12,11 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Home/>
+                <Router>
+                    <Route path="/" exact component={Home}>
+                        {/*<Route path="weather" />*/}
+                    </Route>
+                </Router>
             </Provider>
         );
     }
