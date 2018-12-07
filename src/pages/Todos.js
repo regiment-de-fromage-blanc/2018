80c3ddbfcd54f7c124b12bc585a319465eaaa2b5
Borowy--Vanackere Malo <malo.borowy--vanackere@etu.univ-lyon1.fr>
2018-12-07 02:28:50 +0100

    todos avant de faire nawak commit

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -5,28 +5,55 @@ import { withHandlers } from 'recompose';
 import { firestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import '../App.css';
 import CreateTodo from '../components/todos/CreateTodo';
+import { Row, Grid, Col } from 'react-bootstrap';
+
+const Todos = ({ firestore, todos }) => (
+  <div className="App">
+    <div className="App-todos">
+      <h4>Todos List</h4>
+      <div>
+        {!isLoaded(todos)
+          ? 'Loading'
+          : isEmpty(todos)
+          ? 'Todo list is empty'
+          : todos.map(todo => (
+              <Grid key={todo.id}>
+                <Row className="Todo">
+                  <Row className="TodoHeader">
+                    <Col xs={4} className="TodoCategory">
+                      {todo.category}
+                    </Col>
+                    <Col xs={8} className="TodoRightHeader">
+                      <Row className="TodoSubjectNDate">
+                        <Col xs={6} className="TodoSubject">
+                          {todo.subject}
+                        </Col>
+                        <Col xs={2} className="TodoDate">
+                          {todo.dueDate
+                            ? new Date(
+                                todo.dueDate.seconds
+                              ).toLocaleDateString()
+                            : null}
+                        </Col>
+                      </Row>
+                      <Row className="TodoState">{todo.state}</Row>
+                    </Col>
+                  </Row>
+                  <Row className="Content">{todo.content}</Row>
+                </Row>
+              </Grid>
+            ))}
+      </div>
+      <CreateTodo />
+    </div>
+  </div>
+);
 
 const enhance = compose(
   firestoreConnect([{ collection: 'todos' }]),
   connect(({ firestore }) => ({
     todos: firestore.ordered.todos
   }))
-);
-
-const Todos = ({ firestore, todos }) => (
-  <div className="App">
-    <div className="App-todos">
-      <h4>Todos List Ta mère</h4>
-      <div>
-        {!isLoaded(todos)
-          ? 'Loading'
-          : isEmpty(todos)
-          ? 'Todo list is empty'
-          : todos.map(todo => <div>{todo.subject}</div>)}
-      </div>
-      <CreateTodo />
-    </div>
-  </div>
 );
 
 export default enhance(Todos);
